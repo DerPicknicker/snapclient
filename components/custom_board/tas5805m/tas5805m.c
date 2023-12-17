096050673b8f6917338b3d3091bcd560ddc7ab19
DerPicknicker <louis.welc@web.de>
2023-12-17 19:05:21 +0100

    I2C Driver init works

--- components/custom_board/tas5805m/tas5805m.c
+++ components/custom_board/tas5805m/tas5805m.c
@@ -40,20 +40,8 @@ static const char *TAG = "TAS5805M";
 
 static uint8_t VolumeLevel = 0;
 
+// Default I2C config
 
-
-// static i2c_bus_handle_t i2c_handler;
-
-/*
- * i2c default configuration
- */
-/*static i2c_config_t i2c_cfg = {
-    .mode = I2C_MODE_MASTER,
-    .sda_pullup_en = GPIO_PULLUP_ENABLE,
-    .scl_pullup_en = GPIO_PULLUP_ENABLE,
-    .master.clk_speed = 100000,
-};
-*/
 
 /*
  * Operate fuction of PA
@@ -69,6 +57,26 @@ audio_hal_func_t AUDIO_CODEC_TAS5805M_DEFAULT_HANDLE = {
     .audio_hal_lock = NULL,
     .handle = NULL,
 };
+
+static i2c_config_t i2c_cfg = { 
+    .mode = I2C_MODE_MASTER,
+    .sda_pullup_en = GPIO_PULLUP_ENABLE,
+    .scl_pullup_en = GPIO_PULLUP_ENABLE,
+    .master.clk_speed = I2C_MASTER_FREQ_HZ,
+};
+
+void i2c_master_init() {
+  int i2c_master_port = I2C_MASTER_NUM;
+
+  get_i2c_pins(I2C_NUM_0, &i2c_cfg);
+
+  esp_err_t res = i2c_param_config(i2c_master_port, &i2c_cfg);
+  printf("Driver param setup : %d\n", res);
+  res = i2c_driver_install(i2c_master_port, i2c_cfg.mode,
+                           I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE,
+                           0);
+  printf("Driver installed   : %d\n", res);
+}
 
 esp_err_t tas5805m_read_byte(uint8_t register_name, uint8_t *data)
 {
@@ -101,7 +109,7 @@ esp_err_t tas5805m_read_byte(uint8_t register_name, uint8_t *data)
 
 esp_err_t tas5805m_write_byte(uint8_t register_name, uint8_t value)
 {
-  esp_err_t ret;
+  int ret;
   i2c_cmd_handle_t cmd = i2c_cmd_link_create();
   i2c_master_start(cmd);
   i2c_master_write_byte(cmd, TAS5805M_ADDRESS << 1 | WRITE_BIT, ACK_CHECK_EN);
@@ -116,36 +124,54 @@ esp_err_t tas5805m_write_byte(uint8_t register_name, uint8_t value)
 
 esp_err_t tas5805m_init()
 {
-    int ret;
+  int ret;
+  // Init the I2C-Driver 
+  i2c_master_init();
+  /* Register the PDN pin as output and write 1 to enable the TAS chip */
+  gpio_config_t io_conf;
+  io_conf.intr_type = GPIO_INTR_DISABLE;
+  io_conf.mode = GPIO_MODE_OUTPUT;
+  io_conf.pin_bit_mask = TAS5805M_GPIO_PDN_MASK;
 
-    
-  
-    /* set PDN to 1 */
-    gpio_set_level(TAS5805M_GPIO_PDN, 1);
-    vTaskDelay(100 / portTICK_RATE_MS);
+  io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+  io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+  // log_d("Power down pin: %d", TAS5805M_GPIO_PDN);
+  gpio_config(&io_conf);
+  gpio_set_level(TAS5805M_GPIO_PDN, 0);
+  vTaskDelay(200 / portTICK_RATE_MS);
+  gpio_set_level(TAS5805M_GPIO_PDN, 1);
+  vTaskDelay(200 / portTICK_RATE_MS);
 
-   // LOGI(TAG,"Setting to HI Z");
-    ret = tas5805m_write_byte(TAS5805M_DEVICE_CTRL_2_REGISTER, 0x02);
-    vTaskDelay(100 / portTICK_RATE_MS);
-    if (ret != ESP_OK) return ret;
+  /* set PDN to 1 */
+  gpio_set_level(TAS5805M_GPIO_PDN, 1);
+  vTaskDelay(100 / portTICK_RATE_MS);
 
-    //LOG_I(TAG,"Setting to PLAY");
-    ret = tas5805m_write_byte(TAS5805M_DEVICE_CTRL_2_REGISTER, 0x03);
-    if (ret != ESP_OK) return ret;
-
-    // Check if Bridge-Mode is enabled 
-    #ifdef CONFIG_DAC_OPERATION_MODE
-     uint8_t value = 0;
-     ret = tas5805m_read_byte(TAS5805M_DEVICE_CTRL_1_REGISTER, &value);
-     if (ret != ESP_OK) return ret;
-     value= 0b100; 
-     //log_i("Setting to MONO mode, CTRL_1 = %d", value);        
-     ret = tas5805m_write_byte(TAS5805M_DEVICE_CTRL_1_REGISTER, value);
-     if (ret != ESP_OK) return ret;   
-    #endif
-
-    vTaskDelay(100 / portTICK_RATE_MS);
+  // LOGI(TAG,"Setting to HI Z");
+  ret = tas5805m_write_byte(TAS5805M_DEVICE_CTRL_2_REGISTER, 0x02);
+  vTaskDelay(100 / portTICK_RATE_MS);
+  if (ret != ESP_OK)
     return ret;
+
+  // LOG_I(TAG,"Setting to PLAY");
+  ret = tas5805m_write_byte(TAS5805M_DEVICE_CTRL_2_REGISTER, 0x03);
+  if (ret != ESP_OK)
+    return ret;
+
+// Check if Bridge-Mode is enabled
+#ifdef CONFIG_DAC_OPERATION_MODE
+  uint8_t value = 0;
+  ret = tas5805m_read_byte(TAS5805M_DEVICE_CTRL_1_REGISTER, &value);
+  if (ret != ESP_OK)
+    return ret;
+  value = 0b100;
+  // log_i("Setting to MONO mode, CTRL_1 = %d", value);
+  ret = tas5805m_write_byte(TAS5805M_DEVICE_CTRL_1_REGISTER, value);
+  if (ret != ESP_OK)
+    return ret;
+#endif
+
+  vTaskDelay(100 / portTICK_RATE_MS);
+  return ret;
 }
 
 esp_err_t
@@ -189,12 +215,11 @@ tas5805m_set_mute(bool enable)
   }
 }
 
-esp_err_t tas5805m_deinit(void) {
+esp_err_t tas5805m_deinit(void)
+{
   // TODO
   return ESP_OK;
 }
-
-
 
 esp_err_t
 tas5805m_get_mute(bool *enabled)
@@ -209,15 +234,16 @@ tas5805m_get_mute(bool *enabled)
   }
 }
 
-
 esp_err_t tas5805m_ctrl(audio_hal_codec_mode_t mode,
-                        audio_hal_ctrl_t ctrl_state) {
+                        audio_hal_ctrl_t ctrl_state)
+{
   // TODO
   return ESP_OK;
 }
 
 esp_err_t tas5805m_config_iface(audio_hal_codec_mode_t mode,
-                                audio_hal_codec_i2s_iface_t *iface) {
+                                audio_hal_codec_i2s_iface_t *iface)
+{
   // TODO
   return ESP_OK;
 }
